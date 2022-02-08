02caa05148973d294dc26feb816a939e8dce6cad
Tyler Tierney <tytierney@yahoo.com>
2022-02-08 02:14:20 -0500

    Working on convo item component; state for active rooms is still not working properly

--- client/src/components/ConversationsList/ConvoListItem.tsx
+++ client/src/components/ConversationsList/ConvoListItem.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Icon } from "@chakra-ui/react";
+import { Flex, Text, Icon, Avatar, useColorModeValue } from "@chakra-ui/react";
 import styles from "./ConversationsList.module.css";
 import { MdArrowForwardIos } from "react-icons/md";
 import { useEffect, useState } from "react";
@@ -11,6 +11,7 @@ interface ConvoListItemProps {
 
 const ConvoListItem: React.FC<ConvoListItemProps> = ({ room }) => {
   const [roomInfo, setRoomInfo] = useState<any>({});
+  const liBgColor = useColorModeValue("white", "#23232a");
 
   useEffect(() => {
     const getRoomFromID = async (roomID: string) => {
@@ -34,9 +35,24 @@ const ConvoListItem: React.FC<ConvoListItemProps> = ({ room }) => {
   if (roomInfo === {} || roomInfo == undefined) return null;
 
   return (
-    <button className={styles.roomMenuItem}>
+    <button
+      className={styles.convoListItem}
+      onClick={() => console.log(roomInfo)}
+    >
+      <Avatar size="sm" mr="10px" />
       <Flex className={styles.listItemTextContainer}>
-        <Text className={styles.roomName}>{roomInfo["name"]}</Text>
+        <Text className={styles.liHeader}>{roomInfo["name"]}</Text>
+        <Text
+          as="span"
+          className={styles.convoPreview}
+          _after={{
+            content: `""`,
+            background:
+              "linear-gradient(90deg,rgba(255, 255, 255, 0) 0% #23232a 100%)",
+          }}
+        >
+          Hey so for lunch I was thinking we could go to
+        </Text>
       </Flex>
       <Icon
         as={MdArrowForwardIos}
