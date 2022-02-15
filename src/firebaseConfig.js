0da4d3ee399f6d2d3fa7d0a99edfdb2209d1907f
Tyler Tierney <tytierney@yahoo.com>
2022-02-15 10:51:56 -0500

    Accidentially had firebase emulators active

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -24,9 +24,9 @@ const app = initializeApp(firebaseConfig);
 export const storage = getStorage();
 // export const storageRef = ref(storage);
 export const auth = getAuth(app);
-connectAuthEmulator(auth, "http://localhost:9099");
+// connectAuthEmulator(auth, "http://localhost:9099");
 export const db = getFirestore();
-connectFirestoreEmulator(db, "localhost", 8080);
+// connectFirestoreEmulator(db, "localhost", 8080);
 // const analytics = getAnalytics(app);
 
 export default app;
