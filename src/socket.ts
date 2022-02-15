d3acf9c1842005d8f7bd1f930e765d248aa34876
Tyler Tierney <tytierney@yahoo.com>
2022-02-14 23:06:20 -0500

    Updated socket.ts with new backend url

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,8 @@
 import socketIOClient from "socket.io-client";
 
-const ENDPOINT = "http://192.168.254.32:6066";
+// const ENDPOINT = "http://192.168.254.32:6066";
+const ENDPOINT = "https://chatmosphere-backend.herokuapp.com/";
+
 const socket = socketIOClient(ENDPOINT);
 
 export default socket;
